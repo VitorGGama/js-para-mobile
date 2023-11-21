07534092f2d03f136ac8d2322949a0db67a00ec2
vitor.gama <vitorggam@hotmail.com>
2023-11-21 11:39:07 -0300

    refaturacao modo arrow

--- 02-funcoes.js
+++ 02-funcoes.js
@@ -27,18 +27,22 @@ exemplo2();
 1) Crie uma função (qualquer sintaxe) que receba dois 
 valores numéricos, calcule a diferença entre
 eles e retorne o resultado.*/
-function calcularDiferenca(valor1, valor2) {
-    return valor1 - valor2;
-}
+//function calcularDiferenca(valor1, valor2) {
+    //return valor1 - valor2;
+//}
 /*2) Chame esta função duas vezes passando valores
 diferentes e guardando os resultados em varíaveis de escopo
 global.*/
+
+const calcularDiferenca = (valor1, valor2) => valor1 - valor2;
+ 
 let resultado1 = calcularDiferenca(10, 5);
 let resultado2 = calcularDiferenca(20, 10);
 
 /*3) Mostre os valores processados pela função no terminal.*/
 console.log("Resultado 1:", resultado1);
 console.log("Resultado 2:", resultado2);
+
 
 /* Sintaxe Arrow Function */
 const exemplo3 = () => {
