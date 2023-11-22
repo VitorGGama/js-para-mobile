08951d37e091939d9557d3d49fde22dbb609fc07
vitor.gama <vitorggam@hotmail.com>
2023-11-22 11:12:49 -0300

    import de funcoes

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -6,6 +6,34 @@ para evitar conflitos entre módulos de mesmo nome */
 import { pessoa, livro, alunos as reprovados } from "./modulos/dados.js";
 import alunos from "./modulos/alunos.js";
 
+//importação manual de cada função
+//import {
+//    converteMaiusculas,
+//    converteMinusculas,
+//    formataMoeda
+// } from "./modulos/funcoes.js";
+
+import * as minhasFuncoes from "./modulos/funcoes.js";
+
+console.log( minhasFuncoes.converteMaiusculas(reprovados[2]));
+console.log( minhasFuncoes.converteMinusculas(reprovados[3]));
+
+console.log("-------------");
+
+
+let preco1 = 1200.88;
+let preco2 = 1000000.75;
+let preco3 = 500.8452;
+
+console.log( minhasFuncoes.formataMoeda(preco1));
+console.log( minhasFuncoes.formataMoeda(preco2));
+console.log( minhasFuncoes.formataMoeda(preco3));
+
+
+console.log("-------------");
+
+
+
 console.log(reprovados); // usando o módulo alunos através de apelido
 console.log("-------");
 console.log(alunos); // usando o nome original do módulo
--- modulos/funcoes.js
+++ modulos/funcoes.js
@@ -1,2 +1,18 @@
 //modulos/funcoes
 
+function converteMaiusculas(texto){
+    return texto.toUpperCase();
+}
+
+function converteMinusculas(texto){
+    return texto.toLowerCase();
+}
+
+function formataMoeda(valor){
+    return valor.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    });
+}
+
+export { converteMaiusculas, converteMinusculas, formataMoeda}
