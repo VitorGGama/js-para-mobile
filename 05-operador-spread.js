76c228a6d04047d40ff1a2aaf80eb99c47d2b929
vitor.gama <vitorggam@hotmail.com>
2023-11-22 11:53:59 -0300

    console.log(novosDados);

--- 05-operador-spread.js
+++ 05-operador-spread.js
@@ -4,9 +4,26 @@
 Copiar elementos/dados de um array/objeto
 para dentro de outro array/objeto. */
 
+//Spread com arrays
+
 const bandas = ["Pink Floyd", "Rush", "Yes"];
 const maisBandas = [...bandas, "Slayer", "Nightwish"];
 
-
 console.log(bandas);
 console.log(maisBandas);
+
+//Spread com objetos
+const cliente = {
+    nome: "Seu Madruga",
+    cidade: "São Paulo",
+    idade: 55
+};
+
+const novosDados = {
+    ...cliente, //Copiando as props. do cliente pra cá 
+    estado: "SP",
+    pedido: "123xry",
+    total: 5000.88,
+    idade: 60 //sobreescrevendo valores de propriedades
+};
+console.log(novosDados);
