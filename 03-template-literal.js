6a1e42127e9ccc8e66f04830addc673d72f4e6ac
vitor.gama <vitorggam@hotmail.com>
2023-11-22 08:55:17 -0300

    Concatenação tradicional

---
+++ 03-template-literal.js
@@ -0,0 +1,15 @@
+// 03-template-literal.js
+
+const nome = "Chapolin";
+const sobrenome = "colorado";
+let idade = 25;
+let cidade = "São Paulo";
+let estado = "SP";
+
+/* Concatenação tradicional usando operador +
+Meu nome é Chapolin Colorado, tenho 25 anos e moro atualmente
+na cidade de São Paulo - SP. */
+let mensagem1 = "Meu nome é <b class='destaque'>"+nome+"</b> "+sobrenome+", tenho "+idade+" anos e moro atualmente na cidade de "
++cidade+ " -"+estado+".";
+
+console.log(mensagem1);
