be23379e0b3e19cea9347533ec0971be6abebac3
vitor.gama <vitorggam@hotmail.com>
2023-11-23 09:57:47 -0300

    Destructuring para parâmetros de função

--- 07-desestruturacao.js
+++ 07-desestruturacao.js
@@ -1,54 +1,103 @@
-//07-desestruturacao.js
+// 07-desestruturacao.js
 
 /* Destructuring ou Desestruturação
-Extrair os dados de arrays e objetos para varíaveis/constantes
-individuais*/
+Extrair os dados de arrays e objetos para variáveis/constantes
+individuais */
 
-//Destruturing em arrays
-const alunas = ["Tanaka", "Zimbo", "melissa"];
+// Destructuring em arrays
+const alunas = ["Tanaka", "Zimbo", "Melissa"];
 const [tanaka, zimbo, melissa] = alunas;
-
 
 console.log(tanaka);
 console.log(zimbo);
 console.log(melissa);
 
 const unidades = ["Penha", "Tatuapé", "Itaquera", "São Miguel"];
-const [penha, tatuape, , saomiguel] = unidades;
 
-console.log("-------------------");
+                    // pulando Itaquera
+const [penha, tatuape, , smp] = unidades;
+
+console.log("-------");
+
 console.log(penha);
 console.log(tatuape);
-console.log(saomiguel);
+console.log(smp);
 
-console.log("-------------------");
+console.log("-------");
 
-const [tesla, einsten, newton] = ["Tesla", "Einsten", "Newton"];
+const [tesla, einstein, newton] = ["Tesla", "Einstein", "Newton"];
 
 console.log(tesla);
-console.log(einsten);
+console.log(einstein);
 console.log(newton);
 
-console.log("-------------------");
+console.log("-------");
 
 const recursos = [
-    ["notebook", "TV Led", "Computador desktop"],
+    ["Notebook", "TV Led", "Computador Desktop"],
     texto => texto.toUpperCase()
 ];
-console.log(recursos); //Arrays completo
 
-//destructuring
+console.log(recursos); // Array completo
+
+// Destructuring
 const [produtos, converter] = recursos;
 
-//Acessando através da constante que foi gerada pelo destructuring
-console.log(produtos[1]); //TV led
+// Acessando através da constante que foi gerada pelo destructuring
+console.log(produtos[1]); // TV Led
 
-for (const produto of produtos) {
-    console.log("Produto: " + produto);
+for(const produto of produtos) {
+    console.log("Produto: "+produto);
+}
+
+console.log( converter("tiago") );
+console.log( converter("gelADEira") );
+console.log( converter(produtos[0]) );
+
+console.log("-----------------");
+
+// Destructuring em objetos
+const pessoa = {
+    nome: "Andre",
+    idade: 19,
+    bairro: "Penha",
+    situacao: "crítica"
 };
 
+const { nome, idade, bairro, situacao } = pessoa;
+
+console.log(`O aluno ${nome} está em situação ${situacao} no curso.`);
+console.log(`Mora na ${bairro} e mesmo assim sempre atrasa... :(`);
 
 
-console.log(converter("tiago"));
-console.log(converter("geladeira"));
-console.log(converter(produtos[1]));
+// Criando um apelido para a propriedade codigo
+const { codigo: pedido, cursos, preco } = { 
+    codigo: "123abc", cursos: ["Figma", "Node.js"], preco: 1000 
+};
+
+console.log(pedido); // pedido é um apelido
+console.log(cursos);
+console.log(preco);
+
+console.log("--------");
+
+// Destructuring para parâmetros de função
+function exibirDados(objeto){
+    console.log(`Filme: ${objeto.titulo} - Ano: ${objeto.ano}`);
+}
+
+const filme1 = {
+    titulo: "Vingadores",
+    ano: 2012
+};
+
+const filme2 = {
+    titulo: "Barbie",
+    ano: 2023
+};
+
+exibirDados(filme1);
+exibirDados(filme2);
+
+
+
