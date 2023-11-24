bfceb85d0a2d681857c45e3c67f3b2fa84acbfc7
vitor.gama <vitorggam@hotmail.com>
2023-11-24 09:40:43 -0300

    console.log("Exercicios");

--- 09-filter.js
+++ 09-filter.js
@@ -1,3 +1,4 @@
+import cursos from "./modulos/cursos.js"
 // 09-filter.js
 /* Filter (Filtrar)
 Passa por elementos de um array ( usando a função callback) e 
@@ -27,4 +28,28 @@ const resultados = alunos.filter( aluno => {
     return aluno.startsWith("M") || aluno.endsWith("o")
 });
 console.log(resultados);
+
+//Arrow function
+const resultado = alunos.filter(aluno => aluno.startsWith("M") || aluno.endsWith("o"));
+console.log(resultado);
+
+console.log("------------------");
+
+/* filtrando cursos */ 
+
+//Filtrando pr cursos da categoria Design
+
+const cursoDesign = cursos.filter( curso => curso.categoria == "Design");
+console.log(cursoDesign);
+
+//Filtrando por cursos que não são da categoria Design
+const cursosMenosDesign = cursos.filter(curso => curso.categoria != "Desugn");
+console.log(cursosMenosDesign);
+
+console.log("Exercicios");
+
+const cursoFrontAcima600 = cursos.filter(curso => curso.preco > 600 && curso.categoria == "Front-End" || curso.categoria == "Mobile");
+console.log(cursoFrontAcima600);
+
+
 
